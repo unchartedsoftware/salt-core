46e1a5af069d966f36d5fca47a923679ebbf7bc3
Sean McIntyre <s.mcintyre@xverba.ca>
2015-08-11 16:26:30 -0400

    Altered order of matches in DataFrameUtil based on likelihood

--- src/main/scala/com/uncharted/mosaic/util/DataFrameUtil.scala
+++ src/main/scala/com/uncharted/mosaic/util/DataFrameUtil.scala
@@ -7,17 +7,17 @@ import org.apache.spark.sql.Row
  * Common operations for DataFrames which compose our source data, such as
  * the retrieval of sample rows or the computation of summary statistics.
  */
-object DataFrameUtil {
+object DataFrameUtil extends Serializable {
   def getDouble(col: Int, r: Row): Double = {
     r.get(col) match {
-      case b: Byte => b.toDouble
-      case s: Short => s.toDouble
-      case i: Int => i.toDouble
+      case d: Double => d
+      case t: Timestamp => (t.getTime).toDouble
       case l: Long => l.toDouble
       case f: Float => f.toDouble
-      case d: Double => d
+      case i: Int => i.toDouble
+      case s: Short => s.toDouble
       case d: Date => (d.getTime).toDouble
-      case t: Timestamp => (t.getTime).toDouble
+      case b: Byte => b.toDouble
       case _ => {
         val colType = r.get(col).getClass.getName
         throw new IllegalArgumentException(s"Column $col is non-numeric (type: $colType)")
