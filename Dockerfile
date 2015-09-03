a36162a8b179b64d9d2c824f9a3eb99959ef462e
Sean McIntyre <s.mcintyre@xverba.ca>
2015-09-03 11:02:34 -0400

    Require 100% test coverage

--- Dockerfile
+++ Dockerfile
@@ -4,9 +4,13 @@
 #
 # Runs the Mosaic test suite in a container
 #
-# Usage:
-# docker build -t docker.uncharted.software/mosaic-test .
-# docker run --rm docker.uncharted.software/mosaic-test
+# One-time usage (such as on travis):
+# $ docker build -t docker.uncharted.software/mosaic-test .
+# $ docker run --rm docker.uncharted.software/mosaic-test
+#
+# Dev environment usage:
+# $ docker run -v $(pwd):/opt/mosaic -it docker.uncharted.software/mosaic-test bash
+# container$ ./gradlew
 
 FROM sequenceiq/spark:1.4.0
 MAINTAINER Sean McIntyre <smcintyre@uncharted.software>
--- src/test/scala/com/unchartedsoftware/mosaic/core/analytic/numeric/CountAggregatorSpec.scala
+++ src/test/scala/com/unchartedsoftware/mosaic/core/analytic/numeric/CountAggregatorSpec.scala
@@ -4,11 +4,28 @@ import org.scalatest._
 import com.unchartedsoftware.mosaic.core.analytic.numeric._
 
 class CountAggregatorSpec extends FunSpec {
-
   describe("CountAggregator") {
     describe("#default()") {
       it("should have a default value of 0") {
         assert(CountAggregator.default === 0D)
+      }
+    }
+
+    describe("#add()") {
+      it("should add 1 to the existing count when a new value is passed in") {
+        assert(CountAggregator.add(CountAggregator.default, Some(1)) === 1D)
+        assert(CountAggregator.add(CountAggregator.default, Some("hello")) === 1D)
+      }
+      it("should add 1 to the existing count when a null record is passed in") {
+        assert(CountAggregator.add(CountAggregator.default, None) === 1D)
+      }
+    }
+
+    describe("#merge()") {
+      it("should combine two counts using addition") {
+        var left = Math.random()
+        var right = Math.random()
+        assert(CountAggregator.merge(left, right) === left + right)
       }
     }
   }
