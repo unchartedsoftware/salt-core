05cfda58776b8aaa42a5a38d860d77c4ddc19469
Chris Bethune <cbethune@oculusinfo.com>
2015-12-06 09:20:54 -0500

    adds msys path compatibility

--- test-environment.sh
+++ test-environment.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
 
+local=$(pwd)
+workdir=/opt/salt
+
+if [[ "$OSTYPE" == "msys" ]]; then
+	local="/$local"
+	workdir="/$workdir"
+fi
+
 docker run \
 -p 8080:8080 \
 -p 9999:9999 \
 -e GRADLE_OPTS="-Dorg.gradle.daemon=true" \
--v $(pwd)/src/test/resources/log4j.properties:/usr/local/spark/conf/log4j.properties \
--v $(pwd):/opt/salt \
+-v "$local/src/test/resources/log4j.properties":/usr/local/spark/conf/log4j.properties \
+-v "$local":/opt/salt \
 -it \
---workdir="/opt/salt" \
+--workdir="$workdir" \
 uncharted/sparklet:1.5.2 bash
