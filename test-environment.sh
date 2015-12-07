54d5b2eaa9259bf06bcda9d48840103aa482ac88
Sean McIntyre <s.mcintyre@xverba.ca>
2015-12-07 14:54:05 -0500

    Enhancing compatibility with Windows and IntelliJ [fixes #71] [skip ci]

--- test-environment.sh
+++ test-environment.sh
@@ -4,8 +4,8 @@ docker run \
 -p 8080:8080 \
 -p 9999:9999 \
 -e GRADLE_OPTS="-Dorg.gradle.daemon=true" \
--v $(pwd)/src/test/resources/log4j.properties:/usr/local/spark/conf/log4j.properties \
--v $(pwd):/opt/salt \
+-v /$(pwd)/src/test/resources/log4j.properties:/usr/local/spark/conf/log4j.properties \
+-v /$(pwd):/opt/salt \
 -it \
---workdir="/opt/salt" \
+--workdir="//opt/salt" \
 uncharted/sparklet:1.5.2 bash
