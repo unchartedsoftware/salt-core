c6375ea942a49ae3593b4d1dbfdbd1f63c12582f
Sean McIntyre <s.mcintyre@xverba.ca>
2015-12-07 11:21:45 -0500

    Only exposing necessary ports in test-environment.sh [skip ci]

--- test-environment.sh
+++ test-environment.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 docker run \
---net="host" \
+-p 8080:8080 \
+-p 9999:9999 \
 -e GRADLE_OPTS="-Dorg.gradle.daemon=true" \
 -v $(pwd)/src/test/resources/log4j.properties:/usr/local/spark/conf/log4j.properties \
 -v $(pwd):/opt/salt \
