fbcd090fcde5ad62790cfe0ca640d93d7a2bfa76
Sean McIntyre <s.mcintyre@xverba.ca>
2016-04-01 17:45:21 -0400

    Adding dense mode to SparseArray which kicks in automatically and is transparent to clients.

--- src/main/scala/software/uncharted/salt/core/util/SparseArray.scala
+++ src/main/scala/software/uncharted/salt/core/util/SparseArray.scala
@@ -21,17 +21,24 @@ import scala.collection.mutable.{ArrayLike, Builder, HashMap}
 
 /**
  * An integer-indexed sparse array implementation, currently based on HashMap.
- * Specialized for storing Ints, Longs and Doubles.ss
+ * Specialized for storing Ints, Longs and Doubles
+ * Automatically materializes into a dense array when the number of non-default
+ * values stored exceeds some threshold.
+ * Probably not thread-safe due to lack of locking on materialization.
+ * TODO: https://docs.oracle.com/javase/7/docs/api/java/util/concurrent/locks/ReadWriteLock.html
  *
  * @param size The maximum number of indices in the SparseArray
  * @param default The default value which will appear to be stored at "empty" positions in the SparseArray
- * @param values An initial set of values to place in the SparseArray. Should be fewer than size elements.
+ * @param inValues An initial set of values to place in the SparseArray. Should be fewer than size elements.
+ * @param threshold when density() exceeds this threshold [0,1], this SparseArray will store its values in an
+ *                  array instead of a map. 1/3 by default.
  * @tparam T the type of value being stored in the SparseArray
  */
 class SparseArray[@specialized(Int, Long, Double) T] private[salt] (
   size: Int,
   val default: T,
-  inValues: Map[Int, T]
+  inValues: Map[Int, T],
+  threshold: Float = 1/3F
 )(implicit tag: ClassTag[T])
   extends ArrayLike[T, SparseArray[T]]
   with Builder[T, SparseArray[T]]
@@ -39,17 +46,36 @@ class SparseArray[@specialized(Int, Long, Double) T] private[salt] (
 
   assert (size >= inValues.size)
 
+  private var materialized = false
   private var internalSize = size
-  private var internalValues = new HashMap[Int, T]() ++ inValues
+  private lazy val sparseValues = new HashMap[Int, T]() ++ inValues
+  //scalastyle:off null
+  private var denseValues: Array[T] = null
+  //scalastyle:on null
 
   def this(size: Int, default: T)(implicit tag: ClassTag[T]) = {
     this(size, default, Map[Int, T]())
   }
 
+  private def materialize() = {
+    if (!materialized) {
+      denseValues = Array.fill[T](internalSize)(default)
+      for ((key, value) <- sparseValues) {
+        denseValues.update(key, value)
+      }
+      materialized = true
+      sparseValues.clear()
+    }
+  }
+
   @throws(classOf[ArrayIndexOutOfBoundsException])
   override def apply(idx: Int): T = {
     if (idx < internalSize) {
-      internalValues.getOrElse(idx, default)
+      if (materialized) {
+        denseValues(idx)
+      } else {
+        sparseValues.getOrElse(idx, default)
+      }
     } else {
       throw new ArrayIndexOutOfBoundsException(idx)
     }
@@ -63,28 +89,42 @@ class SparseArray[@specialized(Int, Long, Double) T] private[salt] (
    * @return the ratio of concrete elements to the total virtual size of this SparseArray
    */
   def density(): Float = {
-    internalValues.size / internalSize.toFloat
+    if (materialized) {
+      1
+    } else {
+      sparseValues.size / internalSize.toFloat
+    }
   }
 
   @throws(classOf[ArrayIndexOutOfBoundsException])
   override def update(idx: Int, elem: T): Unit = {
     if (idx >= internalSize) {
       throw new ArrayIndexOutOfBoundsException(idx)
+    } else if (materialized) {
+      denseValues(idx) = elem
     } else if (!elem.equals(default)) {
       // only store non-default values
-      internalValues.put(idx, elem)
+      sparseValues.put(idx, elem)
+      // materialize sparse array into dense array if we're over the threshold
+      if (this.density() > threshold) {
+        this.materialize()
+      }
     } else {
       // we know elem equals default, so wipe out the internally stored value if there was one
-      internalValues.remove(idx)
+      sparseValues.remove(idx)
     }
   }
 
   override def seq: collection.IndexedSeq[T] = {
-    val buff = Array.fill[T](internalSize)(default)
-    for ((key, value) <- internalValues) {
-      buff(key) = value
+    if (materialized) {
+      denseValues
+    } else {
+      val buff = Array.fill[T](internalSize)(default)
+      for ((key, value) <- sparseValues) {
+        buff(key) = value
+      }
+      buff
     }
-    buff
   }
 
   override def newBuilder(): Builder[T, SparseArray[T]] = {
@@ -97,17 +137,24 @@ class SparseArray[@specialized(Int, Long, Double) T] private[salt] (
     internalSize += 1
     // only store non-default values
     if (!elem.equals(default)) {
-      internalValues.put(internalSize-1, elem)
+      sparseValues.put(internalSize-1, elem)
     }
     this
   }
 
   override def clear(): Unit = {
     internalSize = 0
-    internalValues.clear()
+    materialized = false
+    //scalastyle:off null
+    denseValues = null
+    //scalastyle:on null
+    sparseValues.clear()
   }
 
   override def result(): SparseArray[T] = {
+    if (this.density() > threshold) {
+      this.materialize()
+    }
     this
   }
 }
