e124a28839b2c39f872f7e1aee2ffb70b4051820
Nathan Kronenfeld <nkronenfeld@uncharted.software>
2016-10-31 12:42:12 -0400

    Better comments explaining the impact of the non-standard nature of sparse arrays.

--- src/main/scala/software/uncharted/salt/core/util/SparseArray.scala
+++ src/main/scala/software/uncharted/salt/core/util/SparseArray.scala
@@ -25,14 +25,29 @@ import scala.reflect.ClassTag
 
 /**
   * An integer-indexed sparse array implementation, currently based on HashMap.
+  *
   * Specialized for storing Ints, Longs and Doubles
+  *
   * Automatically materializes into a dense array when the number of non-default
   * values stored exceeds some threshold.
+  *
+  * Note that this is <em>not</em> a standard scala sequence.  While the map
+  * method has basically the same meaning, the reduce method <em>only</em> has
+  * the same meaning if the default value passed into the sparse array is a zero,
+  * because reduce only operates on non-defaulted entries.  Also not that this
+  * means that, if given a non-zero default value, reduce will return different
+  * values if an array is materialized and if it is not.  In general, it is
+  * clearly best to use a default value equivalent to zero with respect to any
+  * operations that will be performed on the data.
+  *
   * Probably not thread-safe due to lack of locking on materialization.
+  *
   * TODO: https://docs.oracle.com/javase/7/docs/api/java/util/concurrent/locks/ReadWriteLock.html
   *
   * @param _length The (fixed) length of the array
-  * @param _default The default value used for elements without a specifically set value
+  * @param _default The default value used for elements without a specifically set value.  This value should always
+  *                 be a zero with respect to the operations to be performed on the sparse array.  If it is not, the
+  *                 above warnings about non-standard behavior in the reduce function will come into play.
   * @param _threshold The proportion of elements with non-default values beyond which the array will be materialized
   * @tparam T the type of value being stored in the SparseArray
   */
